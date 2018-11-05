f1c4d7ec7ac8070b540b8747dce2c8a9564d647c
cemo-P <cemo.polat@hs-worms.de>
2018-11-05 23:59:03 +0100

    Es muss gemacht werden: 1- timer ein und ausschalten 2 - aufgabe nach start click starten und in einem schleife packen mit zeit oder button oder eingabe von benutzer organisiert 3- benutzer obergrenze wahl einstellen 4 - ergebnis überprüfen ob richtig oder falsch 5 - punkte sistem einfügen

--- app/src/main/java/com/example/cemo/reko_version_2/AufgabeEinstellen.java
+++ app/src/main/java/com/example/cemo/reko_version_2/AufgabeEinstellen.java
@@ -3,6 +3,8 @@ package com.example.cemo.reko_version_2;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.preference.CheckBoxPreference;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -15,10 +17,10 @@ import java.util.Random;
 
 public class AufgabeEinstellen extends Activity implements View.OnClickListener {
     CheckBox Multiplikation, Addition, Division,Subtraktion;
-    static int bis;
-    int a=1,b=10, x, y, z, s;
+    int a=1,b=10, x, y, z, s=10, operand;
     ArrayList<String> opi = new ArrayList<String>();
-    TextView tvZahl1, tvZahl2, tvOperand, tvGleich;
+    int laenger = opi.size();
+    TextView tvZahl1, tvZahl2, tvOperand, tvGleich, tvTimer;
     Button starten;
     EditText bisi;
     boolean checked;
@@ -38,8 +40,9 @@ public class AufgabeEinstellen extends Activity implements View.OnClickListener 
         tvZahl1 = (TextView)findViewById(R. id. tvZahl1);
         tvOperand = (TextView)findViewById(R. id. tvOperand);
         tvZahl2 = (TextView)findViewById(R. id. tvZahl2);
+        tvTimer = findViewById(R. id. tvTimer);
         tvGleich = (TextView)findViewById(R. id. tvGleich);
-        bisi = (EditText)findViewById(R. id. eTBis);
+        bisi = findViewById(R. id. eTBis);
 
         starten = (Button)findViewById(R. id. btnStarten);
 
@@ -50,17 +53,88 @@ public class AufgabeEinstellen extends Activity implements View.OnClickListener 
         Division.setEnabled(true);
         starten.setOnClickListener(this);
 
+
        zufal();
 
         tvZahl1.setText(String.valueOf(x));
         tvZahl2.setText(String.valueOf(y));
+        bisi.setText(String.valueOf(b));
+
+        if(operand == 0){
+            tvOperand.setText("+");
+        }else if(operand==1){
+            tvOperand.setText("*");
+        }else if(operand == 2){
+            tvOperand.setText("-");
+        }else if(operand == 3){
+            tvOperand.setText("/");
+
+        }
 
 
 
 
     }
 
+    public void setBis(){
+        EditText bisi =(EditText)findViewById(R.id. eTBis);
+        b = Integer.parseInt(bisi.getText().toString());
+    }
 
+    @Override
+    public void onClick(View v) {
+        if(Multiplikation.isChecked() || Addition.isChecked()||Subtraktion.isChecked()||Division.isChecked()){
+            if(Multiplikation.isChecked()){
+                opi.add("*");
+            }else if(Addition.isChecked()){
+                opi.add("+");
+            }else if(Subtraktion.isChecked()){
+                opi.add("-");
+            }else if(Division.isChecked()){
+                opi.add("/");
+            }else if(!Multiplikation.isChecked()){
+                opi.remove("*");
+            }else if(!Addition.isChecked()){
+                opi.remove("+");
+            }else if(!Subtraktion.isChecked()){
+                opi.remove("-");
+            }else if(!Division.isChecked()){
+                opi.remove("/");
+            }
+
+
+            new CountDownTimer(10_000, 1_000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    tvTimer.setText(String.valueOf(s));
+                    s--;
+                }
+
+                @Override
+                public void onFinish() {
+
+                    s = 10;
+                    tvTimer.setText("Zeit ist abgelaufen");
+                }
+            }.start();
+            //Es muss implementiert werden
+
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Bitte Wählen Sie eine Operand aus", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    public void zufal(){
+        Random zuf = new Random();
+
+        x = zuf.nextInt(b - a)+1;
+        y = zuf.nextInt(b -a);
+        operand = zuf.nextInt(4);
+
+    }
 
     public void SelectItem(View view) {
         checked = ((CheckBox) view).isChecked();
@@ -101,29 +175,5 @@ public class AufgabeEinstellen extends Activity implements View.OnClickListener 
 
         }
 
-    }
-    public void setBis(){
-        EditText bisi =(EditText)findViewById(R.id. eTBis);
-        bis = Integer.parseInt(bisi.getText().toString());
-    }
-
-    public void zufal(){
-        Random zuf = new Random();
-        x = zuf.nextInt(b - a)+1;
-        y = zuf.nextInt(b -a);
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        if(Multiplikation.isChecked() || Addition.isChecked()||Subtraktion.isChecked()||Division.isChecked()){
-            //Es muss implementiert werden
-
-        }
-        else
-        {
-            Toast.makeText(getApplicationContext(),"Bitte Wählen Sie eine Operand aus", Toast.LENGTH_LONG).show();
-
-        }
     }
 }
