7957e64bbf43e9fadc3dcbae535796e2ad525f66
cemo-P <cemo.polat@hs-worms.de>
2018-11-07 23:18:02 +0100

    yavas yavas düzelecek

--- app/src/main/java/com/example/cemo/reko_version_2/AufgabeEinstellen.java
+++ app/src/main/java/com/example/cemo/reko_version_2/AufgabeEinstellen.java
@@ -15,15 +15,16 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class AufgabeEinstellen extends Activity implements View.OnClickListener {
+public class AufgabeEinstellen extends Activity {
     CheckBox Multiplikation, Addition, Division,Subtraktion;
-    int a=1,b=10, x, y, z, s=10, operand, eingabe, punkt = 0;
+    int a=1,b=10, x, y, z, s=10, c, operand, eingabe, punkt = 0;
     ArrayList<String> opi = new ArrayList<String>();
     int laenger = opi.size();
     TextView tvZahl1, tvZahl2, tvOperand, tvGleich, tvTimer, tvPunkte;
     Button starten;
     EditText bisi;
     boolean checked;
+    String g;
 
 
 
@@ -33,136 +34,40 @@ public class AufgabeEinstellen extends Activity implements View.OnClickListener 
         setContentView(R.layout.activity_aufgabe_einstellen);
 
 
-        Addition = (CheckBox)findViewById(R. id. cBAddition);
-        Multiplikation = (CheckBox)findViewById(R. id. cBMultiplikation);
-        Subtraktion= (CheckBox)findViewById(R. id. cBSubtraktion);
-        Division = (CheckBox)findViewById(R. id. cBDivision);
-        tvOperand = (TextView)findViewById(R. id. tvOperand);
-        tvPunkte =(TextView)findViewById(R. id.tvPunkt);
+        Addition = findViewById(R. id. cBAddition);
+        Multiplikation = findViewById(R. id. cBMultiplikation);
+        Subtraktion= findViewById(R. id. cBSubtraktion);
+        Division = findViewById(R. id. cBDivision);
+        tvOperand = findViewById(R. id. tvOperand);
+        tvPunkte =findViewById(R. id.tvPunkt);
 
 
 
         tvTimer = findViewById(R. id. tvTimer);
-        tvGleich = (TextView)findViewById(R. id. tvGleich);
+        tvGleich = findViewById(R. id. tvGleich);
         bisi = findViewById(R. id. eTBis);
 
-        starten = (Button)findViewById(R. id. btnStarten);
+        starten = findViewById(R. id. btnStarten);
 
 
         Addition.setEnabled(true);
         Multiplikation.setEnabled(true);
         Subtraktion.setEnabled(true);
         Division.setEnabled(true);
-        starten.setOnClickListener(this);
-        tvPunkte.setText(String.valueOf("Punkte : "+punkt));
-
-
 
 
     }
 
-    public void setBis(){
+    public void setBis(){ //eingabe für die Obergrenze
         EditText bisi =(EditText)findViewById(R.id. eTBis);
         b = Integer.parseInt(bisi.getText().toString());
         bisi.setText(String.valueOf(b));
 
 
     }
-    public void zufal(){
-        Random zuf = new Random();
-
-        x = zuf.nextInt(b - a)+1;
-        y = zuf.nextInt(b -a);
-        operand = zuf.nextInt(4);
-        tvZahl1 = (TextView)findViewById(R. id. tvZahl1);
-        tvZahl1.setText(String.valueOf(x));
-
-        tvZahl2 = (TextView)findViewById(R. id. tvZahl2);
-        tvZahl2.setText(String.valueOf(y));
-        if(operand == 0){
-            z = x + y;
-            tvOperand.setText("+");
-
-
-        }else if(operand==1){
-            z = x * y;
-            tvOperand.setText("*");
-
-        }else if(operand == 2){
-            z = x - y;
-            tvOperand.setText("-");
-
-        }else if(operand == 3){
-            z = x / y;
-            tvOperand.setText("/");
-        }
-
-
-    }
-    public int ergPruf(){
+    public void ergPruf(){  //muss ber erarbeitet werden
         EditText erPruf = (EditText)findViewById(R. id. etErgebnis);
         eingabe = Integer.parseInt(erPruf.getText().toString());
-        if(z == eingabe) {
-            punkt++;
-        }
-        return punkt;
-    }
-
-
-
-    @Override
-    public void onClick(View v) {
-        if(Multiplikation.isChecked() || Addition.isChecked()||Subtraktion.isChecked()||Division.isChecked()){
-            if(Multiplikation.isChecked()){
-                opi.add("*");
-
-            }else if(Addition.isChecked()){
-                opi.add("+");
-
-            }else if(Subtraktion.isChecked()){
-                opi.add("-");
-
-            }else if(Division.isChecked()){
-                opi.add("/");
-
-            }else if(!Multiplikation.isChecked()){
-                opi.remove("*");
-            }else if(!Addition.isChecked()){
-                opi.remove("+");
-            }else if(!Subtraktion.isChecked()){
-                opi.remove("-");
-            }else if(!Division.isChecked()){
-                opi.remove("/");
-            }
-            setBis();
-            zufal();
-
-
-
-            new CountDownTimer(10_000, 1_000) {
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    tvTimer.setText(String.valueOf(s));
-                    s--;
-
-                }
-
-                @Override
-                public void onFinish() {
-
-                    s = 10;
-                    tvTimer.setText("Zeit ist abgelaufen");
-                }
-            }.start();
-            //Es muss implementiert werden
-
-        }
-
-        else
-        {
-            Toast.makeText(getApplicationContext(),"Bitte Wählen Sie eine Operand aus", Toast.LENGTH_LONG).show();
-
-        }
 
     }
 
@@ -206,6 +111,77 @@ public class AufgabeEinstellen extends Activity implements View.OnClickListener 
                 break;
 
         }
+         c = opi.size();
+    }
+    public void zufal(){ //zufallszahlgenerator
+        Random zuf = new Random();
+
+        x = zuf.nextInt(b - a)+1;
+        y = zuf.nextInt(b -a);
+
+        tvZahl1 = (TextView)findViewById(R. id. tvZahl1);
+        tvZahl1.setText(String.valueOf(x));
+
+        tvZahl2 = (TextView)findViewById(R. id. tvZahl2);
+        tvZahl2.setText(String.valueOf(y));
+        operand = zuf.nextInt(c);
+        if(operand == 0){
+            z = x + y;
+            tvOperand.setText("+");
+
+
+        }else if(operand==1){
+            z = x * y;
+            tvOperand.setText("*");
+
+        }else if(operand == 2){
+            z = x - y;
+            tvOperand.setText("-");
+
+        }else if(operand == 3){
+            z = x / y;
+            tvOperand.setText("/");
+        }
+        if(z == eingabe) {
+            punkt++;
+        }
+        tvPunkte.setText(String.valueOf(punkt));
 
     }
+    public void onClick(View v) { //Button aktivierung
+
+        setBis();
+        zufal();
+
+
+        /*Timer start und stop*/
+        if(Multiplikation.isChecked() || Addition.isChecked()||Subtraktion.isChecked()||Division.isChecked()){
+            new CountDownTimer(10_000, 1_000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    tvTimer.setText(String.valueOf(s));
+                    s--;
+
+                }
+
+                @Override
+                public void onFinish() {
+
+                    s = 10;
+                    tvTimer.setText("Zeit ist abgelaufen");
+                }
+            }.start();
+            //Es muss implementiert werden
+
+        }
+
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Bitte Wählen Sie eine Operand aus", Toast.LENGTH_LONG).show();
+
+        }
+
+
+    }
+
 }
